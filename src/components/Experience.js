2eb1b6e087115973ef0bf244de69f3919f018167
eunjeong09 <ejko0911@gmail.com>
2021-10-26 15:47:17 +0900

    Add skill list

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,31 +2,62 @@ import styled from "styled-components";
 
 const Circle = styled.div`{width:50px;height:50px;border-radius:50px;background-color:yellow}`;
 
+const LeftArea = styled.div`{width:50%;float:left}`;
+const RightArea = styled.div``;
+const Title = styled.span`{font-size:20px;font-weight:bold;color:#6a00ff;margin-right:20px}`;
+
 function Experience() {
     return (
         <div>
-            <h1>Experience</h1>
-            <p>대학교 졸업</p>
-            <p>(주)플랜아이 퍼블리셔 인턴 및 수습</p>
-            <p>대전 인재개발원 전자정보통신sw프레임워크 과정 수료</p>
-            <p>(주)마루이엔지 프론트엔드 개발자</p>
+            {/* <LeftArea>
+                <ul>
+                    <li>왼쪽 목록으 교육과 경력을 나누는게 의미있나?</li>
+                </ul>
+            </LeftArea> */}
+            <RightArea>
+                <h1>Experience</h1>
+                <p>
+                    <Title>2015.03</Title>
+                    <span>대전대학교 회계학과 입학</span>
+                </p>
+                <p>
+                    <Title>2019.02</Title>
+                    <span>대전대학교 회계학과, 전자정보통신공학과 졸업</span>
+                </p>
+                <p>
+                    <Title>2018.08 ~ 2019.03</Title>
+                    <span>(주)플랜아이 퍼블리셔 인턴 및 수습</span>
+                </p>
+                <p>
+                    <Title>2019.05 ~ 2019.12</Title>
+                    <span>대전 인재개발원 전자정보통신sw프레임워크 과정 수료</span>
+                </p>
+                <p>
+                    <Title>2020.02 ~ 2021.07</Title>
+                    <span>(주)마루이엔지 프론트엔드 개발자</span>
+                </p>
 
 
-            <Circle>
-                1
-            </Circle>
-            <Circle>
-                2
-            </Circle>
-            <Circle>
-                3
-            </Circle>
-            <Circle>
-                4
-            </Circle>
-            <Circle>
-                5
-            </Circle>
+
+                <Circle>
+                    1
+                </Circle>
+                <Circle>
+                    2
+                </Circle>
+                <Circle>
+                    3
+                </Circle>
+                <Circle>
+                    4
+                </Circle>
+                <Circle>
+                    5
+                </Circle>
+
+            </RightArea>
+
+
 
         </div>
     )
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,3 +1,8 @@
+import styled from "styled-components";
+
+const Circle = styled.div`{}`;
+const SkillWrap = styled.div`{width:30%;margin-left:10px;margin-right:10px;display:inline-block;float:left}`;
+const SkillText = styled.p`{font-size:20px;font-weight:bold;color:#6a00ff;text-align:center}`;
 function Skill() {
     return (
         <div>
@@ -6,12 +11,17 @@ function Skill() {
             <h1>Skill</h1>
             <p>스킬을 애니메이션 넣어서 퍼센트로 표현하면 좋을듯</p>
             <div>
-                <span>html5</span>
-                <span>css3</span>
-                <span>javascript</span>
-                <span>React</span>
-                <span>jquery</span>
-                <span>?</span>
+                <SkillWrap>
+                    <div>
+                        
+                    </div>
+                    <SkillText>html5</SkillText>
+                </SkillWrap>
+                <SkillWrap><SkillText>css3</SkillText></SkillWrap>
+                <SkillWrap><SkillText>javascript</SkillText></SkillWrap>
+                <SkillWrap><SkillText>React</SkillText></SkillWrap>
+                <SkillWrap><SkillText>jquery</SkillText></SkillWrap>
+                <SkillWrap><SkillText>?</SkillText></SkillWrap>
             </div>
         </div>
     )
