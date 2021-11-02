06bd3ad1ac5345cc83cfb91d91eeb61fca15d9eb
eunjeong09 <ejko0911@gmail.com>
2021-11-02 16:28:22 +0900

    Add photo on main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,19 @@
 import { FaGithub, FaPhoneAlt, FaVimeoV, FaEnvelope, FaChild } from 'react-icons/fa';
-
+import styled from 'styled-components';
+import photoImage from '../images/eunjeong.png';
 const icons = {
     verticalAlign:"bottom"
 }
-
+const photo = {
+    width:"170px",
+    height:"150px"
+}
 function Main() {
     return (
         <div>
-            <h1>main</h1>
-            <p>어쩌구 저쩌구 프론트엔드 개발자 고은정입니다.</p>
+            <p>어쩌구 저쩌구 프론트엔드 개발자 <h1>고은정</h1>입니다.</p>
+            
+            <img style={photo} src={photoImage} />
             <p><FaChild style={icons}/> 1996.09.06</p>
             <p><FaPhoneAlt style={icons}/> 010-9985-6676</p>
             <p><FaEnvelope style={icons}/> ejko0911@gmail.com</p>
