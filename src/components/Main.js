768d37c8cd9e9b5172363846d821e09cd2f62801
eunjeong09 <ejko0911@gmail.com>
2021-11-02 14:54:05 +0900

    Add favicon in html, Add icons in main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
+import { FaGithub, FaPhoneAlt, FaVimeoV, FaEnvelope, FaChild } from 'react-icons/fa';
+
+const icons = {
+    verticalAlign:"bottom"
+}
+
 function Main() {
     return (
         <div>
             <h1>main</h1>
             <p>어쩌구 저쩌구 프론트엔드 개발자 고은정입니다.</p>
-
-            <span>핸드폰</span>
-            <span>이메일</span>
-            <span>git</span>
-            <span>블로그?</span>
+            <p><FaChild style={icons}/> 1996.09.06</p>
+            <p><FaPhoneAlt style={icons}/> 010-9985-6676</p>
+            <p><FaEnvelope style={icons}/> ejko0911@gmail.com</p>
+            <p><FaGithub style={icons}/> https://github.com/eunjeong09</p>
+            <p><FaVimeoV style={icons}/> https://velog.io/@eunjeong</p>
         </div>
 
         // 블로그 이메일 링크
