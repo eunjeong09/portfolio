8fda778b410ea643fea9378c18355dbd1a078345
고은정 <ejko0911@gmail.com>
2022-03-06 11:23:06 +0900

    Develop 컴포넌트 추가 및 임시커밋

---
+++ src/renewal/Develop.js
@@ -0,0 +1,24 @@
+function Develop(){
+    return(
+        <div className="develop">
+            <div className="box">
+                이미지
+                Velog
+                주제
+                <div className="button">
+                    (링크)More
+                </div>
+            </div>
+            <div className="box">
+                악수 이미지??
+                협업
+                마이포폴그램
+                <div className="button">
+                    (링크)More
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Develop;
